9ff142e6770ea0bd047b989d168506380c10f533
rune <70787260+bibliothec@users.noreply.github.com>
2022-06-22 15:54:09 +0900

    Delete elpa directory

--- elpa/helm-flycheck-0.4/helm-flycheck-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from helm-flycheck.el  -*- no-byte-compile: t -*-
-(define-package "helm-flycheck" "0.4" "Show flycheck errors with helm" '((dash "2.12.1") (flycheck "28") (helm-core "1.9.8")) :commit "a15e62a6432c165c4f2c17388686873383400d7d" :authors '(("Yasuyuki Oka" . "yasuyk@gmail.com")) :maintainer '("Yasuyuki Oka" . "yasuyk@gmail.com") :keywords '("helm" "flycheck") :url "https://github.com/yasuyk/helm-flycheck")
